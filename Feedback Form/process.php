712a611943b48369e9a55898a10a7256f12a7f86
Adrian Gaile Singh <adriangailee@gmail.com>
2024-03-05 11:27:16 +0800

    Implement nullish coalescing operator instead of isset

--- Feedback Form/process.php
+++ Feedback Form/process.php
@@ -1,19 +1,29 @@
+<?php
+// using ?? Nullish coalescing operator  to check if $_POST["full_name"] is set.
+$full_name = $_POST["full_name"] ?? null;
+$score = $_POST["score"] ?? null;
+$course = $_POST["course"] ?? null;
+$reason = $_POST["reason"] ?? null;
+?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Feedback Form </title>
 </head>
+
 <body>
     <header>
         <h1>Submitted Entry</h1>
     </header>
-    <p>Name: <?= $_POST["full_name"]?></p>
-    <p>Course Title: <?= $_POST["course"]?></p>
-    <p>Given Score: <?= $_POST["score"]?></p>
-    <p>Reason: <?= $_POST["reason"]?></p>
+    <p>Name: <?= $full_name ?> </p>
+    <p>Course Title: <?= $course ?></p>
+    <p>Given Score: <?= $score ?></p>
+    <p>Reason: <?= $reason ?></p>
     <button><a href="./form.php">click me</a></button>
 </body>
+
 </html>
