d7cc7c6e934e0b35306b8b7933796b5f07709718
Adrian Gaile Singh <adriangailee@gmail.com>
2024-03-07 00:23:48 +0800

    modified files

--- Free Coupons/index.php
+++ Free Coupons/index.php
@@ -4,58 +4,33 @@ session_start();
 if (!isset($_SESSION["remaining_coupons"])) {
     $_SESSION["remaining_coupons"] = 10;
 }
+
+if ($_SESSION["remaining_coupons"] == 0) {
+    header("Location: reset.php");
+    exit();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Free Coupons</title>
 </head>
-
 <body>
     <main>
         <header>
             <h1>Welcome customer! 🚀 </h1>
-            <p><?= $_SESSION["remaining_coupons"]?></p>
+            <p><?= $_SESSION["remaining_coupons"] ?></p>
         </header>
-
-            <?php
-            if ((isset($_SESSION["remaining_coupons"])) && $_SESSION["remaining_coupons"] > 0) {
-            ?>
-            <p>We're giving away <span>free coupons</span>
+        <p>We're giving away <span>free coupons</span>
         <p>as a token of appreciation for the first <?= $_SESSION["remaining_coupons"] ?> customer(s).</p>
         <form action="process.php" method="post">
             <label for="name">Kindly type your name:</label>
             <input type="text" name="submit">
             <input type="submit">
         </form>
-    <?php
-            }
-    ?>
-
-    <?php
-    if (isset($_SESSION["remaining_coupons"]) && $_SESSION["remaining_coupons"] == 0) {
-    ?>
-        <div>
-            <header>
-                <h1>Sorry! </h1>
-            </header>
-            <p>You're ran out of voucher</p>
-            <form action="process.php" method="post">
-                <input type="hidden" name="action" value="reset">
-                <input type="submit">
-            </form>
-
-        </div>
-    <?php
-    }
-    ?>
-
-
     </main>
 </body>
-
 </html>
--- Free Coupons/process.php
+++ Free Coupons/process.php
@@ -5,22 +5,31 @@ if (!isset($_SESSION["remaining_coupons"])) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_SESSION["claimed_coupons"])) {
-        $_SESSION["claimed_coupons"]++;
+    if (isset($_POST["action"]) && $_POST["action"] == "reset") {
+    unset($_SESSION["remaining_coupons"]);
+    unset($_SESSION["claimed_coupons"]);
+    header("Location: index.php");
+    exit();
+    }
+    else if  ($_POST["action"] == "claim_again") {
+        unset($_SESSION["discount_code"]);
     }
     else {
-        $_SESSION["claimed_coupons"] = 1;
+        if (isset($_SESSION["claimed_coupons"])) {
+            $_SESSION["claimed_coupons"]++;
+        }
+        else {
+            $_SESSION["claimed_coupons"] = 1;
+        }
+        $_SESSION["remaining_coupons"] = 10 - $_SESSION["claimed_coupons"];
+
+        $discount_code = substr(md5(mt_rand()), 0, 9);
+        $_SESSION["discount_code"] = $discount_code;
+
+        header("Location: new_page.php");
+        exit();
     }
-
-    $_SESSION["remaining_coupons"] = 10 - $_SESSION["claimed_coupons"];
 }
-
-if (isset($_POST["action"]) && $_POST["action"] == "reset") {
-    $_SESSION["remaining_coupons"] = 10;
-}
-
-
-
 header("Location: index.php");
 exit();
 ?>
