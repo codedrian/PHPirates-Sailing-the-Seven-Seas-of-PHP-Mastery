7a10424c85920997e05a94794b6975deb964862b
Adrian Gaile Singh <adriangailee@gmail.com>
2024-03-08 18:57:07 +0800

    Ensure ['money'] is initialized if not set

--- Money Button Game/index.php
+++ Money Button Game/index.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
-$money = 500;
+if (!isset($_SESSION["money"])) {
+    $_SESSION["money"] = 500;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,17 +14,17 @@ $money = 500;
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<h1>Your money: <?= $money ?></h1>
+<h1>Your money: <?= $_SESSION["money"]?></h1>
 
 <div class="form-container">
-    <form action="process.php" class="form-group">
+    <form action="process.php" method="post" class="form-group">
         <input type="hidden" name="action" value="low_risk">
         <label for="low_risk">Low Risk</label>
         <input type="submit" value="Bet">
         <p>by -25 to 100</p>
     </form>
     <form action="process.php" method="post" class="form-group">
-        <input type="hidden" name="moderate_risk" value="moderate_risk">
+        <input type="hidden" name="action" value="moderate_risk">
         <label for="moderate_risk">Moderate Risk</label>
         <input type="submit" value="Bet">
         <p>by -100 up tp 1000</p>
@@ -38,6 +41,34 @@ $money = 500;
         <input type="submit" value="Bet">
         <p>by -3000 to 5000</p>
     </form>
+</div>
+<div>
+
+    <?php
+    if (isset($_SESSION["bet"])) {
+    ?>
+        <h3>Current Bet:</h3>
+        <p>You pushed <?= $_SESSION["bet"] ?></p>
+    <?php
+    }
+    ?>
+
+    <?php
+    if(!empty($_SESSION["betArr"])){
+    ?>
+    <h3>Previous Bet:</h3>
+    <ul>
+        <?php
+        foreach($_SESSION["betArr"] as $bet ) {
+        ?>
+        <li> <?=$bet["date"]?> You have pushed <?= $bet["bet"] ?> with the value <?= $bet["bet_result"] ?> of Your current money now is <?= $bet["money"] ?>
+        </li>
+        <?php
+        }
+        ?>
+    <?php
+    }
+    ?>
 </div>
 </body>
 </html>
