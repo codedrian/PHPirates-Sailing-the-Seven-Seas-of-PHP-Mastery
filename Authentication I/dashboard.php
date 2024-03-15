04a065e3978351f55373a4d8f9c4acbc357748aa
Adrian Gaile Singh <adriangailee@gmail.com>
2024-03-15 23:24:23 +0800

    Initial commit

---
+++ Authentication I/dashboard.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+if ((!isset($_SESSION['is_logged_in']) || $_SESSION['is_logged_in'] !== TRUE)) {
+    header('Location: index.php');
+    exit();
+}
+if (isset($_POST['logout'])) {
+    session_destroy();
+    header('Location: index.php');
+    exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <header>
+        <h1>WELCOME</h1>
+    </header>
+    <form method="post">
+        <input type="submit" name="logout" value="logout">
+    </form>
+</body>
+</html>
