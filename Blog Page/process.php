460552dbe3177203b2be4be5a9c5fb90a2e04506
Adrian Gaile Singh <adriangailee@gmail.com>
2024-03-19 13:06:32 +0800

    Refactor user registration and add login logic

--- Blog Page/process.php
+++ Blog Page/process.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once("connection.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["action"]) && $_POST["action"] == "submit_registration") {
         //NOTE: Sanitation and validation for firstname and lastname
@@ -34,6 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $errors[] = "Please enter your password";
         } else {
             $sanitized_password = (isset($_POST["password"])) ? filter_var($_POST["password"], FILTER_SANITIZE_SPECIAL_CHARS) : "";
+            $hashed_password = password_hash($sanitized_password, PASSWORD_BCRYPT);
             if (empty($_POST["confirm_password"])) {
                 $errors[] = "Please confirm your password";
             } else if ($_POST["password"] !== $_POST["confirm_password"]) {
@@ -46,5 +48,72 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             header("Location: sign_in.php");
             exit();
         }
+        else {
+            $query = "INSERT INTO users (first_name, last_name, email, password)
+                        VALUES (?, ?, ?, ?)";
+            $statement = $connection->prepare($query);
+            $statement -> bind_param("ssss", $sanitized_first_name, $sanitized_last_name, $sanitized_email, $hashed_password);
+            $statement -> execute();
+            $statement -> close();
+            $_SESSION["is_logged_in"] = TRUE;
+            header("Location: dashboard.php");
+            exit();
+        }
     }
+    //NOTE: For log in
+    if (isset($_POST["action"]) && $_POST["action"] == "submit_login") {
+        if (strlen($_POST["email"]) == 0) {
+            $errors[] = "Please enter your email address";
+        } else {
+            $sanitized_email = (isset($_POST["email"])) ? filter_var($_POST["email"], FILTER_SANITIZE_EMAIL) : "";
+            if (!filter_var($sanitized_email, FILTER_VALIDATE_EMAIL)) {
+                $errors[] = "{$sanitized_email} is not a valid email address";
+            }
+        }
+        if (strlen($_POST["password"]) == 0) {
+            $errors[] = "Please enter your password";
+        }
+        //Note: Authentication
+        if (!empty($errors)) {
+            $_SESSION['errors'] = $errors;
+            header("Location: log_in.php");
+            exit();
+        } else {
+            $email = (isset($_POST["email"])) ? $_POST["email"] : "";
+            $confirm_password = (isset($_POST["password"])) ? $_POST["password"] : "";
+
+            $query = "SELECT first_name, last_name, password FROM users WHERE email = ?";
+            $statement = $connection -> prepare($query);
+            $statement -> bind_param("s", $email);
+            $statement -> execute();
+            $statement -> bind_result($first_name, $last_name, $hashed_password);
+            $statement->fetch();
+            $statement->close();
+
+            $message = [];
+            if (password_verify($confirm_password, $hashed_password)) {
+                //NOTE: Store the datas form the database to session variable
+                $_SESSION["is_logged_in"] = TRUE;
+                $_SESSION["first_name"] = $first_name;
+                $_SESSION["last_name"] = $last_name;
+                header("Location: dashboard.php");
+                exit();
+            }
+            else {
+                $message[] = "User not found!";
+                $_SESSION["message"] = $message;
+                header("Location: log_in.php");
+                exit();
+            }
+        }
+    }
+    if (isset($_POST["action"]) && $_POST["action"] == "logout") {
+        session_destroy();
+        header("Location: sign_in.php");
+        exit();
+    }
+} else {
+    header("Location: sign_in.php");
+    exit();
 }
+//accounts:
